5f212d65880bdc2a916cebc84fb9d9b9bb21fde4
josungwong <jsw13160@gmail.com>
2024-07-02 15:44:59 +0900

    으에에에ㅔㅇ

--- main.js
+++ main.js
@@ -13,8 +13,12 @@ let taskList = []
 addButton.addEventListener("click",addTask)
 
 function addTask(){
-    let taskContent = taskInput.value
-    taskList.push(taskContent)
+    let task = {
+        id:randomIDGenerate(),
+        taskContent: taskInput.value,
+        isComplete:false
+    }
+    taskList.push(task)
     console.log(taskList)
     render()
 }
@@ -22,15 +26,49 @@ function addTask(){
 function render(){
     let resultHTML = ''
     for(let i = 0; i<taskList.length; i++){
-        resultHTML += `<div class="task">
-                    <div>${taskList[i]}</div>
+        if(taskList[i].isComplete == true){
+            resultHTML +=`<div class="task">
+                    <div class="task-done">${taskList[i].taskContent}</div>
                     <div>
-                        <button>Check</button>
-                        <button>Delete</button>
+                        <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
+                        <button onclick="deleteTask('${taskList[i].id}')">Delete</button>
                     </div>
                 </div>`
+        } else{
+        resultHTML += `<div class="task">
+                    <div>${taskList[i].taskContent}</div>
+                    <div>
+                        <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
+                        <button onclick="deleteTask('${taskList[i].id}')">Delete</button>
+                    </div>
+                </div>`
+                }
     }
 
-
+    
     document.getElementById("task-board").innerHTML = resultHTML
+}
+
+function toggleComplete(id){
+    for(let i = 0; i < taskList.length; i++){
+        if(taskList[i].id == id){
+            taskList[i].isComplete = !taskList[i].isComplete;
+            break
+        }
+    }
+    render()
+    console.log(taskList)
+}
+function randomIDGenerate(){
+    return Math.random().toString(36).substr(2, 16);
+    
+}
+function deleteTask(id){
+    for(let i = 0; i<taskList.length;i++){
+        if(taskList[i].id == id){
+            taskList.splice(i,1)
+            break
+        }
+    }
+    render()
 }
